4747d740d3191b6bd6cde996d0f3ae20fc3c8338
nathanclevenger <nateclev@gmail.com>
2021-03-20 08:41:58 -0500

    Some style updates

--- pages/wiki/[topic].js
+++ pages/wiki/[topic].js
@@ -3,11 +3,11 @@ import cheerio from 'cheerio'
 
 // export const config = { amp: true }
 
-export default function Home({infobox}) {
+export default function Home({title, infobox, navbox}) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,11 +15,16 @@ export default function Home({infobox}) {
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           
-
           <table className="infobox" dangerouslySetInnerHTML={{__html: infobox}} />
             
-
         </div>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          
+          <table className="navbox" dangerouslySetInnerHTML={{__html: navbox}} />
+            
+        </div>
+
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
@@ -42,12 +47,16 @@ export async function getStaticProps({params}) {
   const res = await fetch(`https://wiki.metad.workers.dev/wiki/${params.topic}`)
   
   const $ = cheerio.load(await res.text())
+  const title = $('#firstHeading').text()
   const infobox = $('table.infobox').html()  //.replaceAll('href="/wiki/','href="/')
-  console.log(infobox)
+  const navbox = $('div.navbox').html()
+  console.log(title)
 
   return {
     props: {
+      title,
       infobox,
+      navbox,
     },
     revalidate: 86400
   }
